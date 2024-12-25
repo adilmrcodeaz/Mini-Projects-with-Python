e37a038d3321a00e2284740ea4275e54a683fdcf
Adil Rahimov <133156253+adilmrcodeaz@users.noreply.github.com>
2024-12-25 16:04:36 +0400

    Update Basic_Calculator.py

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,19 +1,19 @@
 def add(a,b):
     answer = a+b
-    print(str(a)+ "+" + str(b)+ "=" + str(answer))
+    print(str(a)+ " + " + str(b)+ " = " + str(answer))
 
 
 def sub(a,b):
     answer = a - b
-    print(str(a) + "-" + str(b) + "=" + str(answer))
+    print(str(a) + " - " + str(b) + " = " + str(answer))
 
 def mul(a,b):
     answer = a*b
-    print(str(a)+ "*"+ str(b)+ "=" + str(answer))
+    print(str(a)+ " * "+ str(b)+ " = " + str(answer))
 
 def div(a,b):
     answer = a/b
-    print(str(a)+"/"+str(b)+ "=" + str(answer))
+    print(str(a)+" / "+str(b)+ " = " + str(answer))
 
 
 print("A. Addition")
